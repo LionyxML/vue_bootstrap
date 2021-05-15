9f8a3bfaae2c24dd648098f74431c739b1a40e22
LionyxML <rahul.juliato@gmail.com>
2021-05-15 09:09:48 -0300

    Created setupe and skeleton.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container align-h="center">
+      <b-row align-v="center">
+        <b-col md="4">
+          <b-card
+            title="Titulo do Cartão"
+            img-src="https://picsum.photos/200/200"
+            img-alt=""
+            img-top
+            tag="article"
+            style="max-width:20rem"
+            class="mb-2">
+            <b-card-text>
+              Abc isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro
+            </b-card-text>
+            <b-button variant="primary"> Veja agora!
+            </b-button>
+          </b-card>
+        </b-col>
+        <b-col md="4">
+          <b-card
+            title="Titulo do Cartão"
+            img-src="https://picsum.photos/200/200"
+            img-alt=""
+            img-top
+            tag="article"
+            style="max-width:20rem"
+            class="mb-2">
+            <b-card-text>
+              Abc isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro
+            </b-card-text>
+            <b-button variant="primary"> Veja agora!
+            </b-button>
+          </b-card>
+        </b-col>
+        <b-col md="4">
+          <b-card
+            title="Titulo do Cartão"
+            img-src="https://picsum.photos/200/200"
+            img-alt=""
+            img-top
+            tag="article"
+            style="max-width:20rem"
+            class="mb-2">
+            <b-card-text>
+              Abc isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro
+            </b-card-text>
+            <b-button variant="primary"> Veja agora!
+            </b-button>
+          </b-card>
+        </b-col>
+        <b-col md="4">
+          <b-card
+            title="Titulo do Cartão"
+            img-src="https://picsum.photos/200/200"
+            img-alt=""
+            img-top
+            tag="article"
+            style="max-width:20rem"
+            class="mb-2">
+            <b-card-text>
+              Abc isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro
+            </b-card-text>
+            <b-button variant="primary"> Veja agora!
+            </b-button>
+          </b-card>
+        </b-col>
+      </b-row>
+  </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
