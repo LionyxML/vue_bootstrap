f5fb97a42e86365a8c10084a0d7e94acac02a8fb
LionyxML <rahul.juliato@gmail.com>
2021-05-15 10:43:25 -0300

    Fixed alignment horizontal. Added pagination.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="home">
-    <b-container align-h="center">
+    <b-container align="center">
       <b-row align-v="center">
         <Cartao v-for="cartao in cartoes" :key="cartao.id" :name="cartao.name">
         </Cartao>
       </b-row>
+        <b-pagination align="center"
+          v-model="currentPage"
+          :total-rows="rows"
+          :per-page="perPage"
+          first-text="⏮"
+          prev-text="⏪"
+          next-text="⏩"
+          last-text="⏭"
+          class="mt-4"
+        ></b-pagination>
   </b-container>
   </div>
 </template>
@@ -22,7 +32,10 @@ export default {
   },
   data() {
     return {
-      cartoes: []
+      cartoes: [],
+      currentPage: 1,
+      rows: 1,
+      perPage: 3
     }
   },
   methods: {
@@ -31,6 +44,7 @@ export default {
         const res = await fetch("dados.json");
         const val = await res.json();
         this.cartoes = val;
+        this.rows = this.cartoes.length;
         console.log(val);
       } catch (err) {
         console.log(err);
