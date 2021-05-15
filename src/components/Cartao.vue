3359dff1ebc65c25134bbdb0e698cfc650eddb6d
LionyxML <rahul.juliato@gmail.com>
2021-05-15 09:49:54 -0300

    Made Card a component

---
+++ src/components/Cartao.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-col>
+      <b-card
+        title="Titulo do Cartão"
+        img-src="https://picsum.photos/200/200"
+        img-alt=""
+        img-top
+        tag="article"
+        style="max-width:20rem"
+        class="mb-2">
+        <b-card-text>
+          Abc isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro
+        </b-card-text>
+        <b-button variant="primary"> Veja agora!
+        </b-button>
+      </b-card>
+    </b-col>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,82 +2,22 @@
   <div class="home">
     <b-container align-h="center">
       <b-row align-v="center">
-        <b-col md="4">
-          <b-card
-            title="Titulo do Cartão"
-            img-src="https://picsum.photos/200/200"
-            img-alt=""
-            img-top
-            tag="article"
-            style="max-width:20rem"
-            class="mb-2">
-            <b-card-text>
-              Abc isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro
-            </b-card-text>
-            <b-button variant="primary"> Veja agora!
-            </b-button>
-          </b-card>
-        </b-col>
-        <b-col md="4">
-          <b-card
-            title="Titulo do Cartão"
-            img-src="https://picsum.photos/200/200"
-            img-alt=""
-            img-top
-            tag="article"
-            style="max-width:20rem"
-            class="mb-2">
-            <b-card-text>
-              Abc isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro
-            </b-card-text>
-            <b-button variant="primary"> Veja agora!
-            </b-button>
-          </b-card>
-        </b-col>
-        <b-col md="4">
-          <b-card
-            title="Titulo do Cartão"
-            img-src="https://picsum.photos/200/200"
-            img-alt=""
-            img-top
-            tag="article"
-            style="max-width:20rem"
-            class="mb-2">
-            <b-card-text>
-              Abc isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro
-            </b-card-text>
-            <b-button variant="primary"> Veja agora!
-            </b-button>
-          </b-card>
-        </b-col>
-        <b-col md="4">
-          <b-card
-            title="Titulo do Cartão"
-            img-src="https://picsum.photos/200/200"
-            img-alt=""
-            img-top
-            tag="article"
-            style="max-width:20rem"
-            class="mb-2">
-            <b-card-text>
-              Abc isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro isso aquilo outro
-            </b-card-text>
-            <b-button variant="primary"> Veja agora!
-            </b-button>
-          </b-card>
-        </b-col>
+        <Cartao/>
+        <Cartao/>
+        <Cartao/>
+        <Cartao/>
       </b-row>
   </b-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import Cartao from '@/components/Cartao.vue'
 
 export default {
   name: 'Home',
   components: {
-
+    Cartao
   }
 }
 </script>
